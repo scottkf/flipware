ca165ae326f791d71c45afbf42cac5f4f9b99039
Scott Tesoriere <scott@keepflippin.com>
2010-09-01 06:52:16 -0400

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flipware::Application.config.secret_token = 'dab8fb1b32974f80dfbd5221b84ad34178ed066b732411c41c97940a42873440244543e5a6917284147c3651fbd644ccaf24b0482c67f30cb092096592737c59'
